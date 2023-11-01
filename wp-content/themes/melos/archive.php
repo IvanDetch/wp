b4ff082cdbf91b5f0df32a585ee5b1d7a622b844
IvanDetch <kmrakvi@gmail.com>
2023-11-01 05:04:42 +0300

    Update archive.php

--- wp-content/themes/melos/archive.php
+++ wp-content/themes/melos/archive.php
@@ -7,70 +7,52 @@
 
 get_header(); ?>
 
-			<?php if( have_posts() ): ?>
+<?php if (have_posts()): ?>
 
-				<div id="container">
-					<div class="breadcrumbs">
-				<?php echo wpcourses_breadcrumb( ' / ' ); ?>
-					<div class="row-info-title">
-						<div class="br-title-page"><?php single_term_title() ?></div>
-					</div>
-					</div>
-					<div class="news-block-inside">
-				<?php while( have_posts() ): the_post(); ?>
-					
-					<?php 
-$categories = get_the_category();
-$classes = array();
-foreach($categories as $category) {
-    $classes[] = $category->slug;
-}
-?>	
-					
-					<div class="simple-news-el <?php echo join(' ', $classes) ?>">
-						
-					<!--<article id="post-<?php //the_ID(); ?>" <?php //post_class('blog-article'); ?>>-->
-						
-						
-						<div class="item-badge <?php echo join(' ', $classes) ?>">
-							<span>
-							</span>
-            			</div>	
-						<div class="exc-news-date"><?php the_date('j F Y'); ?></div>	
-							<div class="exc-action-name"><?php the_title(); ?></div>
-						
-						<div class="news-plitka-background"><?php echo get_the_post_thumbnail( $post->ID, 'medium'); ?></div>						
-						<a class="news-plitka-el-href" href="<?php echo get_permalink(); ?>"></a>
-						
-						<!--<header class="entry-header<?php //thinkup_input_stylelayout_class1(); ?>">-->
+    <div id="container">
+        <div class="breadcrumbs">
+            <?php echo wpcourses_breadcrumb(' / '); ?>
+            <div class="row-info-title">
+                <div class="br-title-page"><?php single_term_title() ?></div>
+            </div>
+        </div>
+        <div class="news-block-inside">
+            <?php while (have_posts()): the_post(); ?>
 
-							<?php //echo thinkup_input_blogimage(); ?>
-							<!--<div class="entry-content<?php //thinkup_input_stylelayout_class2(); ?>">-->
+                <?php
+                $categories = get_the_category();
+                $classes = array();
+                foreach ($categories as $category) {
+                    $classes[] = $category->slug;
+                }
+                ?>
 
-							<?php //thinkup_input_blogtitle(); ?>
-							<?php //thinkup_input_blogmeta(); ?>
-							<?php //thinkup_input_blogtext(); ?>
+                <div class="simple-news-el <?php echo join(' ', $classes) ?>">
 
-						<!--</div>-->
-							
-						<!--</header>-->
+                    <div class="item-badge <?php echo join(' ', $classes) ?>">
+			<span>
+			</span>
+                    </div>
+                    <div class="exc-news-date"><?php the_date('j F Y'); ?></div>
+                    <div class="exc-action-name"><?php the_title(); ?></div>
+                    <div class="news-plitka-background"><?php echo get_the_post_thumbnail($post->ID, 'medium'); ?></div>
+                    <a class="news-plitka-el-href" href="<?php echo get_permalink(); ?>"></a>
+                    <div class="clearboth"></div>
 
-						<div class="clearboth"></div>
+                </div>
 
-					<!--</article>--><!-- #post-<?php //get_the_ID(); ?> -->
+            <?php endwhile; ?>
+        </div>
+    </div>
+    <div class="clearboth"></div>
 
-					</div>
+    <?php the_posts_pagination(); ?>
 
-				<?php endwhile; ?>
-				</div>
-				</div><div class="clearboth"></div>
+<?php else: ?>
 
-				<?php the_posts_pagination(); ?>
+    <?php get_template_part('no-results', 'archive'); ?>
 
-			<?php else: ?>
-
-				<?php get_template_part( 'no-results', 'archive' ); ?>		
-
-			<?php endif; wp_reset_postdata(); ?>
+<?php endif;
+wp_reset_postdata(); ?>
 
 <?php get_footer() ?>
